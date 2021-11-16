c20fe144b7cebb7162266daa1907382034054890
Aaryan-palit <74129575+Aaryan-palit@users.noreply.github.com>
2021-11-16 13:07:51 +0530

    Add files via upload
    This is the program to find the last digit of the sum of squares of first n fibonacci numbers ((F0)^2+(F!)^2+(F2)^2+.......+(Fn)^2). Here 0<=n<=10^18.
    Input=73 Output=1, Input= 1234567890 Output=0.

---
+++ fibonacci_sum_squares.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+int fibonacci_sum_squares_naive(long long n) {
+    if (n <= 1)
+        return n;
+    long long previous = 0;
+    long long current  = 1;
+    long long sum      = 1;
+    for (long long i = 0; i < n - 1; ++i) {
+        long long tmp_previous = previous;
+        previous = current;
+        current = tmp_previous + current;
+        sum += current * current;
+    }
+    return sum % 10;
+}
+
+long long find_pisano(long long m) {
+    // function to find the pisano period of a number m
+    long long a = 0, b = 1, c = a + b;
+    for (int i = 0; i < m * m; i++) {
+        c = (a + b) % m;
+        a = b;
+        b = c;
+        if (a == 0 && b == 1) return i + 1;
+    }
+}
+
+long long fibonacci_huge_efficient(long long n, long long m) {
+    // to find the Fn mod m where Fn is the nth Fibonacci term which can be a huge number
+    long long rem = n % find_pisano(m);
+
+    long long x = 0;
+    long long y = 1;
+
+    long long r = rem;
+
+    for (int i = 1; i < rem; i++) {
+        r = (x + y) % m;
+        x = y;
+        y = r;
+    }
+
+    return r % m;
+}
+
+int main() {
+    long long n,a,b,c;
+    std::cin >> n;
+    a= fibonacci_huge_efficient(n,10);
+    b= fibonacci_huge_efficient(n+1,10);
+    c= (a*b)%10;
+    // std::cout << fibonacci_sum_squares_naive(n);
+    std::cout << c;
+}
